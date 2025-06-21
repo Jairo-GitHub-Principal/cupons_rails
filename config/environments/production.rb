de2a4b5550ca5855d02aa03e2f43c0f7b43b3b82
Jairo-GitHub-Principal <jc200507@gmail.com>
2025-06-21 19:43:38 -0300

    adicionando  configuração no arquivo production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.assets.compress = true
 
   # Compressor JS e CSS
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
